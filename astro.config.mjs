b3420d3ab37e4b8cf686d933c8e71d1886bc2a68
jbrinkman <joe.brinkman@improving.com>
2025-09-03 11:09:09 -0400

    Add Mermaid support for diagrams in documentation
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,67 +1,72 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import mermaid from "astro-mermaid"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://valkey.io",
-  base: "/valkey-glide-docs",
-  integrations: [
-    starlight({
-      components: {
-        Header: "./src/components/Header.astro",
-        MobileMenuToggle: "./src/components/MobileMenuToggle.astro",
-      },
-      title: "Valkey Glide",
-      logo: {
-        src: "./src/assets/valkey-logo.svg",
-      },
-      customCss: [
-        // Relative path to your custom CSS file
-        "./src/styles/custom.css",
-      ],
-      favicon: "/favicon-32x32.png",
-      editLink: {
-        baseUrl: "https://github.com/valkey-io/valkey-glide-docs/edit/main/",
-      },
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/valkey-io/valkey-glide",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Getting Started",
-          autogenerate: { directory: "getting-started" },
-        },
-        {
-          label: "Concepts",
-          collapsed: true,
-          autogenerate: { directory: "concepts" },
-        },
-        {
-          label: "How-To Guides",
-          collapsed: true,
-          autogenerate: { directory: "how-to" },
-        },
-        {
-          label: "Languages",
-          collapsed: true,
-          autogenerate: { directory: "languages" },
-        },
-        {
-          label: "Migration",
-          collapsed: true,
-          autogenerate: { directory: "migration" },
-        },
-        {
-          label: "Reference",
-          collapsed: true,
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+	site: "https://valkey.io",
+	base: "/valkey-glide-docs",
+	integrations: [
+		mermaid({
+			theme: "default",
+			autoTheme: true
+		}),
+		starlight({
+			components: {
+				Header: "./src/components/Header.astro",
+				MobileMenuToggle: "./src/components/MobileMenuToggle.astro",
+			},
+			title: "Valkey Glide",
+			logo: {
+				src: "./src/assets/valkey-logo.svg",
+			},
+			customCss: [
+				// Relative path to your custom CSS file
+				"./src/styles/custom.css",
+			],
+			favicon: "/favicon-32x32.png",
+			editLink: {
+				baseUrl: "https://github.com/valkey-io/valkey-glide-docs/edit/main/",
+			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/valkey-io/valkey-glide",
+				},
+			],
+			sidebar: [
+				{
+					label: "Getting Started",
+					autogenerate: { directory: "getting-started" },
+				},
+				{
+					label: "Concepts",
+					collapsed: true,
+					autogenerate: { directory: "concepts" },
+				},
+				{
+					label: "How-To Guides",
+					collapsed: true,
+					autogenerate: { directory: "how-to" },
+				},
+				{
+					label: "Languages",
+					collapsed: true,
+					autogenerate: { directory: "languages" },
+				},
+				{
+					label: "Migration",
+					collapsed: true,
+					autogenerate: { directory: "migration" },
+				},
+				{
+					label: "Reference",
+					collapsed: true,
+					autogenerate: { directory: "reference" },
+				},
+			],
+		}),
+	],
 });
