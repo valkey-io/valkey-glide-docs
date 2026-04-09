e7aefe5d8ba96cc4a47f39d6830fbca6f8db39f6
Alex Le <alex.le@improving.com>
2026-04-09 09:18:10 -0700

    cleanup
    Signed-off-by: Alex Le <alex.le@improving.com>

--- doc-gen/build-docs.sh
+++ doc-gen/build-docs.sh
@@ -7,8 +7,8 @@ set -e  # Exit on any error
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "Generating client api docs..."
-# "$CURR_DIR/build-node-docs.sh"
-# "$CURR_DIR/build-java-docs.sh"
+"$CURR_DIR/build-node-docs.sh"
+"$CURR_DIR/build-java-docs.sh"
 "$CURR_DIR/build-python-docs.sh"
 
 echo "Waiting parallel doc generation to complete..."
