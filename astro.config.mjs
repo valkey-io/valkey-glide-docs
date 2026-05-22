fa5e4eae7153aafac74aaa9b081599886c98cb97
Alex Le <alex.le@improving.com>
2026-05-22 10:14:11 -0700

    moved ha to concept
    Signed-off-by: Alex Le <alex.le@improving.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -147,7 +147,6 @@ export default defineConfig({
             "how-to/installation",
             "how-to/client-initialization",
             "how-to/connection-management",
-            "how-to/high-availability",
             "how-to/publish-and-subscribe-messages",
             "how-to/send-batch-commands",
             "how-to/synchronous-connection",
